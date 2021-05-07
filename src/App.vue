41dcaf0692ed4b7b1807a1a89b5e9d3fb40bf015
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-05-07 12:11:46 +0200

    composition api on app

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         Twotter
       </div>
       <div class="navigation__user">
-        {{ user.username }}
+        {{ state.user.username }}
       </div>
     </nav>
     <UserProfile />
@@ -13,16 +13,21 @@
 </template>
 
 <script>
+import { reactive } from 'vue'
 import UserProfile from "./components/UserProfile"
 export default {
   components: {
     UserProfile
   },
-  data(){
-    return {
-      user: {
+  setup() {
+    const state = reactive({
+       user: {
         username: "_UncleCycha_"
       }
+    })
+
+    return {
+      state
     }
   }
 }
